c859423ca0d6f4d55f115d197e36dd74576ec248
shobannah <116910257+shobannah@users.noreply.github.com>
2023-03-27 19:17:22 -0400

    updated through

--- models/index.js
+++ models/index.js
@@ -16,19 +16,15 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_tags'
+  through: 
+    ProductTag,
+    foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'tag_for_products'
+  through:
+    ProductTag,
+    foreignKey: 'tag_id'
 });
 module.exports = {
   Product,
